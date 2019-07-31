205ec7ecdcbac03bab9f47db78d9319007350f14
Sheldon Perkins <csperkins18@gmail.com>
2019-07-31 18:29:13 -0400

    <Alaphabet array and Count variables>

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Computer Choices
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Count Variables
+var wins=0;
+var losses=0;
+var guesses=0;
+
